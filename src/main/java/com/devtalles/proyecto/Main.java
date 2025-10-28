b9af01cb0641de8c8ed5f4ebb5109ceb87359f21
CAndresSierra <camilofullstack@gmail.com>
2025-10-27 21:31:07 -0500

    refactor(core): Implement log parsing and summary
    Refactors the log processing logic to parse log files,
    extract data and generate summaries. Adds LogParser
    and LogSummary classes. Updates Main to handle log file
    reading and summary generation.

--- src/main/java/com/devtalles/proyecto/Main.java
+++ src/main/java/com/devtalles/proyecto/Main.java
@@ -2,8 +2,7 @@ package com.devtalles.proyecto;
 
 import java.io.File;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("🚀 Iniciando análisis de logs...");
--- src/main/java/com/devtalles/proyecto/log/model/LogSummary.java
+++ src/main/java/com/devtalles/proyecto/log/model/LogSummary.java
@@ -1,4 +1,29 @@
 package com.devtalles.proyecto.log.model;
 
+import java.util.Map;
+import java.util.Set;
+
 public class LogSummary {
+    private int totalEntries;
+    private int errorCount;
+    private Set<String> uniqueUsers;
+    private double averageResponseTime;
+    private Map<Integer, Long> errorCountByCode;
+
+    public LogSummary(int totalEntries, int errorCount, Set<String> uniqueUsers, double averageResponseTime, Map<Integer, Long> errorCountByCode) {
+        this.totalEntries = totalEntries;
+        this.errorCount = errorCount;
+        this.uniqueUsers = uniqueUsers;
+        this.averageResponseTime = averageResponseTime;
+        this.errorCountByCode = errorCountByCode;
+    }
+
+    @Override
+    public String toString() {
+        return  "totalEntries=" + totalEntries +
+                "errorCount=" + errorCount +
+                "uniqueUsers=" + uniqueUsers +
+                "averageResponseTime=" + averageResponseTime +
+                "errorCountByCode=" + errorCountByCode;
+    }
 }
